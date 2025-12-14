7396b8a406005cadcc6572805e75fea096420a80
marinduque-programming <marinduqueprogramming@gmail.com>
2025-12-14 13:43:04 +0800

    Create method-project-design.py

---
+++ phase-1/method-project-design.py
@@ -0,0 +1,8 @@
+Mechanical / Structural:
+The inclusion or exclusion of the indicated feature (component) will be determined solely by the Client. The Client is fully responsible for all necessary fabrications, installments, adjustments and modifications for the mechanical (structural) component of AlitapTech System.
+1.	One (1) central, circular or rectangular (square) post, for main body.
+2.	One (1) circular or rectangular (square) base as soil container.
+3.	Two (2) to eight (8) bamboo posts of plant vine and light.
+4.	One (1) waterproofed control box for main unit with camera hole.
+5.	One (1) waterproofed power box for solar peripherals.
+6.	One (1) adjustable attachment for solar panel fixture.
