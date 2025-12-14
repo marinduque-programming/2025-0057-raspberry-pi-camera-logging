26a68bc760dc7a02f628fe4e39697a644fdc3c92
marinduque-programming <marinduqueprogramming@gmail.com>
2025-12-14 14:18:47 +0800

    Update method-project-design.py

--- phase-1/method-project-design.py
+++ phase-1/method-project-design.py
@@ -1,9 +1,44 @@
-Mechanical / Structural:
+<--Table of Contents-->
+A. Project Goals / Focus
+B. Method / Summary
+C. Method / Project Design
 
+<--Contents-->
+A.	Project Goals / Focus: Draft Only
+1.	Overall structure effectiveness (morning glory plant, mist control, flash pattern).
+2.	Efficiency of solar energy system.
+3.	Effectiveness, efficiency, accuracy, reliability of automated monitoring system at night.
+4.	Effectiveness, efficiency, accuracy, reliability of real-time logging system.
+5.	Effectiveness, efficiency, accuracy, reliability of image capture system.
+6.	Effectiveness, efficiency, accuracy, reliability of mist control system.
+7.	Effectiveness, efficiency, accuracy, reliability of flash pattern control.
+
+B.	Method / Summary: Draft Only
+Electronics / Electrical:
+1.	Solar energy system assembly.
+Software / Programming:
+1.	Automated monitoring at night.
+2.	Real-time logging system.
+3.	Raspberry Pi control and data processing.
+4.	Image capture system.
+5.	Morning glory plant (MGP) mist control.
+6.	Light flash pattern control.
+Mechanical / Structural:
+1.	Overall structure (morning glory plant, mist control, flash pattern).
+2.	Fixture for solar energy system.
+3.	Camera box.
+
+C.	Method / Project Design
+Electronics / Electrical:
+  </>
+
+Software / Programming:
+  </>
+
+Mechanical / Structural:
 The inclusion or exclusion of the indicated feature (component) will be determined solely by the Client. 
 The Client is fully responsible for all necessary fabrications, installments, adjustments and modifications 
 for the mechanical (structural) component of Solar-powered Automated Camera System.
-
 1.	Overall structure (morning glory plant, mist control, flash pattern).
 1.1.	One (1) central, circular or rectangular (square) post, for main body.
 1.2.	One (1) circular or rectangular (square) base as soil container.
