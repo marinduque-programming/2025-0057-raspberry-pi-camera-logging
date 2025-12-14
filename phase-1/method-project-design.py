c1735ff7e15ae03acc954a4b586e911c69cf95a8
marinduque-programming <marinduqueprogramming@gmail.com>
2025-12-14 14:05:22 +0800

    Update method-project-design.py

--- phase-1/method-project-design.py
+++ phase-1/method-project-design.py
@@ -1,8 +1,15 @@
 Mechanical / Structural:
-The inclusion or exclusion of the indicated feature (component) will be determined solely by the Client. The Client is fully responsible for all necessary fabrications, installments, adjustments and modifications for the mechanical (structural) component of AlitapTech System.
-1.	One (1) central, circular or rectangular (square) post, for main body.
-2.	One (1) circular or rectangular (square) base as soil container.
-3.	Two (2) to eight (8) bamboo posts of plant vine and light.
-4.	One (1) waterproofed control box for main unit with camera hole.
-5.	One (1) waterproofed power box for solar peripherals.
-6.	One (1) adjustable attachment for solar panel fixture.
+
+The inclusion or exclusion of the indicated feature (component) will be determined solely by the Client. 
+The Client is fully responsible for all necessary fabrications, installments, adjustments and modifications 
+for the mechanical (structural) component of Solar-powered Automated Camera System.
+
+1.	Overall structure (morning glory plant, mist control, flash pattern).
+1.1.	One (1) central, circular or rectangular (square) post, for main body.
+1.2.	One (1) circular or rectangular (square) base as soil container.
+1.3.	Two (2) to eight (8) bamboo posts of morning glory and light.
+2.	Fixture for solar energy system.
+2.1.	One (1) adjustable attachment for solar panel fixture.
+2.2.	One (1) waterproofed power box for solar peripherals.
+3.	Camera box.
+3.1.	One (1) waterproofed control box for main unit with camera hole.
